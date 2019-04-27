416d7168174fc638211a0f2584eae7a20647ae24
Gabriel Trompiz <gab.tc@icloud.com>
2019-04-27 19:53:33 -0400

    release of the release

--- node/middlewares/commentsFilter.js
+++ node/middlewares/commentsFilter.js
@@ -9,7 +9,7 @@ module.exports.checkOwnershipInManga = (req, res, next) => {
                 next()
             }
             else {
-                res.status(403).send("You are not the owner of the comment.")
+                res.status(403).send({ status: 403, message: "You are not the owner of the comment." })
             }
         }
     })
@@ -18,13 +18,13 @@ module.exports.checkOwnershipInManga = (req, res, next) => {
 module.exports.checkOwnershipInChapter = (req, res, next) => {
     const commentId = req.body.commentId
     commentsHelper.getChapterSingleComment(commentId).then(result => {
-        if(result === null) { res.status(404).send("Comment doesn't exists.") }
+        if(result === null) { res.status(404).send({ status: 404, message: "Comment doesn't exists." }) }
         else {
             if(result === req.user.id) {
                 next()
             }
             else {
-                res.status(403).send("You are not the owner of the comment.")
+                res.status(403).send({ status: 403, message: "You are not the owner of the comment." })
             }
         }
     })
